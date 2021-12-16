fa158b8c2007f418369d1aee6dd7a6f358bb9dd4
acallahan97 <callahanamanda877@gmail.com>
2021-12-16 11:10:37 -0700

    initial commit

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    class BankAccount
+    {
+        private double balance = 0.00;
+        public void Deposit (double Amount)
+        {
+            balance += Amount;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+        public void Withdraw(double Amount)
+        {
+            balance -= Amount;
+        }
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much money would you like to deposit?");
+            var userInput = double.Parse(Console.ReadLine());
+            BankAccount balance = new BankAccount();
+            balance.Deposit(userInput);
+            Console.WriteLine($"Your new balance is {balance.GetBalance()}");
+            Console.WriteLine("How much money would you like to withdraw?");
+            var userWithdraw = double.Parse(Console.ReadLine());
+            balance.Withdraw(userWithdraw);
+            Console.WriteLine($"Your new balance is {balance.GetBalance()}");
+
+        }
+
+
+
+    }
+}
